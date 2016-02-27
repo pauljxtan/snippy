de28415deb3f6baf0d7003467c983caf0c9c0573
Paul J.X. Tan <pjxtan@gmail.com>
2016-02-27 12:43:42 -0500

    Minor reformatting

--- database.py
+++ database.py
@@ -11,22 +11,15 @@ EXAMPLE_LANG = "Python"
 EXAMPLE_TITLE = "Prints hello world"
 EXAMPLE_CODE = "def hello_world():\n    print \"Hello, world!\""
 
+
 def initialize_database(verbose=False):
     connection = sqlite3.connect(DB_NAME)
 
     cursor = connection.cursor()
-    create_command = (
-"""
-CREATE TABLE %s (
-    creation_date DATETIME,
-    type TEXT,
-    language TEXT,
-    title TEXT,
-    code TEXT
-    )
-"""
-        % TABLE_NAME)
-    if verbose: print create_command
+    create_command = ("CREATE TABLE %s (creation_date DATETIME, type TEXT, "
+                      "language TEXT, title TEXT, code TEXT)" % TABLE_NAME)
+    if verbose:
+        print create_command
     cursor.execute(create_command)
 
     insert_snippet(cursor, EXAMPLE_TYPE, EXAMPLE_LANG, EXAMPLE_TITLE,
@@ -35,13 +28,16 @@ CREATE TABLE %s (
     connection.commit()
     connection.close()
 
+
 def insert_snippet(cursor, snippet_type, snippet_lang, snippet_title,
                    snippet_code, verbose=False):
     command = ("INSERT INTO %s VALUES (DATETIME(), '%s', '%s', '%s', '%s')"
                % (TABLE_NAME, snippet_type, snippet_lang, snippet_title,
                   snippet_code))
-    if verbose: print command
+    if verbose:
+        print command
     cursor.execute(command)
+
 
 def print_table_rows():
     """For debugging only"""
@@ -55,6 +51,10 @@ def print_table_rows():
 
     connection.close()
 
+
 if __name__ == '__main__':
+    import os
+
+    os.remove("snippy.db")
     initialize_database(True)
     print_table_rows()
