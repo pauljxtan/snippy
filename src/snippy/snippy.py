0cb2001f64ff2e5a82d05292cb1836cbf4c7f338
Paul Tan <pjxtan@gmail.com>
2016-03-27 12:15:19 -0400

    Refactoring

--- src/snippy/snippy.py
+++ src/snippy/snippy.py
@@ -3,26 +3,22 @@ import ttk
 import database as db
 
 DB_FILENAME = "snippy.db"
-TABLE_NAME = "snippy"
 
 
 class SnippyGui(ttk.Frame):
-    def __init__(self, parent, db_conn):
+    def __init__(self, parent, db_conn, table_name="snippy", verbose=False):
         ttk.Frame.__init__(self, parent)
         self.parent = parent
         self.conn = db_conn
+        self.table_name = table_name
+        self.verbose = verbose
 
         self.tree = self.make_tree()
         self.tree.pack(fill=tk.BOTH, expand=True)
 
-        self.notebook = ttk.Notebook(self.parent)
-        test_frame = ttk.Frame()
-        test_text = tk.Text(test_frame)
-        test_text.insert(tk.END, "FRAME TEXT")
-        test_text.config(state=tk.DISABLED)
-        test_text.pack()
-        self.notebook.add(test_frame, text="TAB TEXT")
-        self.notebook.pack(fill=tk.BOTH, expand=True)
+        self.notebook, self.code_text_frame, self.code_text \
+            = self.make_notebook()
+        self.display_code_in_notebook(1)
 
     def make_tree(self):
         """
@@ -43,11 +39,30 @@ class SnippyGui(ttk.Frame):
         """
         Inserts all existing data into the tree.
         """
-        rows = db.get_all_rows(self.conn, TABLE_NAME)
+        rows = db.get_all_rows(self.conn, self.table_name)
+        print rows
         for i, row in enumerate(rows):
+            print row
             tree.insert("", i, text="", values=row)
 
         return tree
+
+    def make_notebook(self):
+        notebook = ttk.Notebook(self.parent)
+        code_text_frame = ttk.Frame()
+        code_text = tk.Text(code_text_frame)
+        code_text.config(state=tk.DISABLED)
+        code_text.pack()
+        notebook.add(code_text_frame, text="TAB TEXT")
+        notebook.pack(fill=tk.BOTH, expand=True)
+
+        return notebook, code_text_frame, code_text
+
+    def display_code_in_notebook(self, row_id):
+        code = db.get_unique_elem(self.conn, self.table_name, row_id, 'code',
+                                  self.verbose)
+        self.code_text.insert(tk.END, "TEST")
+        self.code_text.insert(tk.END, code)
 
 
 def center(win):
@@ -64,13 +79,13 @@ def center(win):
 
 
 def main():
-    db.init_db(DB_FILENAME, TABLE_NAME, True)
+    db.init_db(DB_FILENAME, "snippy", True)
     db_conn = db.get_connection(DB_FILENAME)
 
     root = tk.Tk()
     # root.geometry('640x480')
     root.title("Snippy")
-    SnippyGui(root, db_conn).pack()
+    SnippyGui(root, db_conn, "snippy", True)
     root.mainloop()
 
 
