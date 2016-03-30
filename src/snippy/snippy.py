c51aa5407fbfcbab471cb4b5bc76243ab1aaad67
Paul Tan <pjxtan@gmail.com>
2016-03-30 16:19:12 -0400

    Set SnippyDB object as attribute of SnippyGUI

--- src/snippy/snippy.py
+++ src/snippy/snippy.py
@@ -1,16 +1,16 @@
 import Tkinter as tk
 import ttk
-import database as db
+from database import SnippyDB
 
 DB_FILENAME = "snippy.db"
+TABLE_NAME = "snippy"
 
 
 class SnippyGui(ttk.Frame):
-    def __init__(self, parent, db_conn, table_name="snippy", verbose=False):
+    def __init__(self, parent, db, verbose=False):
         ttk.Frame.__init__(self, parent)
-        self.parent = parent
-        self.conn = db_conn
-        self.table_name = table_name
+        self._parent = parent
+        self._db = db
         self.verbose = verbose
 
         self.tree = self.make_tree()
@@ -24,31 +24,27 @@ class SnippyGui(ttk.Frame):
         """
         Builds the data tree.
         """
-        tree = ttk.Treeview(self.parent)
+        tree = ttk.Treeview(self._parent)
         tree['columns'] = ('creation_date', 'type', 'lang', 'title')
         tree.heading('creation_date', text="Creation date")
         tree.heading('type', text="Snippet type")
         tree.heading('lang', text="Language")
         tree.heading('title', text="Title")
 
-        tree = self.init_data(tree)
-
-        return tree
+        return self.init_data(tree)
 
     def init_data(self, tree):
         """
         Inserts all existing data into the tree.
         """
-        rows = db.get_all_rows(self.conn, self.table_name)
-        print rows
+        rows = self._db.get_all_rows()
         for i, row in enumerate(rows):
-            print row
             tree.insert("", i, text="", values=row)
 
         return tree
 
     def make_notebook(self):
-        notebook = ttk.Notebook(self.parent)
+        notebook = ttk.Notebook(self._parent)
         code_text_frame = ttk.Frame()
         code_text = tk.Text(code_text_frame)
         code_text.config(state=tk.DISABLED)
@@ -59,8 +55,7 @@ class SnippyGui(ttk.Frame):
         return notebook, code_text_frame, code_text
 
     def display_code_in_notebook(self, row_id):
-        code = db.get_unique_elem(self.conn, self.table_name, row_id, 'code',
-                                  self.verbose)
+        code = self._db.get_unique_elem(row_id, 'code')
         self.code_text.insert(tk.END, "TEST")
         self.code_text.insert(tk.END, code)
 
@@ -79,13 +74,12 @@ def center(win):
 
 
 def main():
-    db.init_db(DB_FILENAME, "snippy", True)
-    db_conn = db.get_connection(DB_FILENAME)
+    db = SnippyDB(DB_FILENAME, TABLE_NAME, clobber=True, verbose=True)
 
     root = tk.Tk()
     # root.geometry('640x480')
     root.title("Snippy")
-    SnippyGui(root, db_conn, "snippy", True)
+    SnippyGui(root, db, verbose=True)
     root.mainloop()
 
 
