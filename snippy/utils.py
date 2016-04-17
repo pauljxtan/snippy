86bb6fa42c946aa8db71770c3dc4fc24e73030bf
Paul Tan <pjxtan@gmail.com>
2016-04-16 21:06:31 -0400

    Initial commit; added function for centering Tkinter window

---
+++ snippy/utils.py
@@ -0,0 +1,11 @@
+def center(win):
+    """
+    Centers the window on the screen.
+    @param win: Tkinter window
+    """
+    win.update_idletasks()
+    width = win.winfo_width()
+    height = win.winfo_height()
+    x = (win.winfo_screenwidth() // 2) - (width // 2)
+    y = (win.winfo_screenheight() // 2) - (height // 2)
+    win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
